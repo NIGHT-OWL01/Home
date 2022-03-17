491ea8f0ef1af02ec1fe22aef9ec348615c0b164
ganesh <ganesh.pawar@krishikhoj.com>
2022-03-17 22:46:41 +0530

    updated settings for gdal

--- Home/settings.py
+++ Home/settings.py
@@ -17,7 +17,7 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","Home.settings")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
