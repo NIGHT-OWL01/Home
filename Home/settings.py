135c9207a4b175ad869651bcfd0bc39bcd993bc3
ganesh <ganesh.pawar@krishikhoj.com>
2022-03-17 22:40:21 +0530

    updated settings for gdal

--- Home/settings.py
+++ Home/settings.py
@@ -152,7 +152,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
